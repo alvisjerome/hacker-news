c949eca3068444980dc5173cf62c7f560524d538
alvisjerome <alvisjerome@gmail.com>
2023-06-14 18:43:44 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Alvis Jerome on 14/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
